d40a152803e385d134e5016342d8e2c02313e54a
Amin Ansari <devansari@yahoo.com>
2023-12-12 06:20:40 +0330

    The hompage.css file is added and some context is added to the Hompage.jsx too

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,3 +1,19 @@
-const HomePage = () => {};
+//Components and other pages
+import ResponsiveContainer from "../components/UI/ResponsiveContainer";
+
+//Css files and other assets
+import "./HomePage.css";
+
+const HomePage = () => {
+  return (
+    <ResponsiveContainer>
+      <h1 className="home-page-welcome-title">Welcome to React events</h1>
+      <p className="home-page-p">
+        Navigate to the <span>Events</span> page to see all the events
+        available!
+      </p>
+    </ResponsiveContainer>
+  );
+};
 
 export default HomePage;
